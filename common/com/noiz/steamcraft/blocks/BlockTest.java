aa5ac952706f1c31f9c82abdaebb627516b499ac
diadistis <bsotirid@gmail.com>
2014-07-05 03:31:01 +0300

    Added tile entity for test block

--- common/com/noiz/steamcraft/blocks/BlockTest.java
+++ common/com/noiz/steamcraft/blocks/BlockTest.java
@@ -1,11 +1,21 @@
 package com.noiz.steamcraft.blocks;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
+import com.noiz.steamcraft.entities.tiles.TEBoiler;
 
-public class BlockTest extends Block {
+import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+public class BlockTest extends Block implements ITileEntityProvider {
 
 	public BlockTest(Material material) {
 		super(1234, material);
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World world) {
+		return new TEBoiler();
 	}
 }
